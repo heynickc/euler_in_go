f8948fd4938272098779f7147c45a42ac847b432
unknown <nchamberlain@BSBZGV1.beltman.local>
2014-08-06 15:06:03 -0400

    Optimization of Problem 5 with least common multiples and greatest common denominators, but I don't get that yet

--- euler_5/5.go
+++ euler_5/5.go
@@ -5,20 +5,39 @@ import (
 )
 
 func main() {
-	divisors := []int{
-		10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+	// divisors := []int{
+	// 	10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+	// }
+	// for i := 1; ; i++ {
+	// 	evenDivisors := make([]int, 0)
+	// 	for _, num := range divisors {
+	// 		factorOfN := i % num
+	// 		if factorOfN == 0 {
+	// 			evenDivisors = append(evenDivisors, num)
+	// 		}
+	// 	}
+	// 	if len(evenDivisors) == 11 {
+	// 		fmt.Printf("%v is divisible by %v\n", i, evenDivisors)
+	// 		break
+	// 	}
+	// }
+
+	// Optimization?
+	var factors = [...]int{2, 3, 2, 5, 2, 7, 3, 11, 13, 2, 17, 19} // GCD or LCM?
+	var num int = 1
+	for key, _ := range factors {
+		num *= factors[key]
 	}
-	for i := 1; ; i++ {
-		evenDivisors := make([]int, 0)
-		for _, num := range divisors {
-			factorOfN := i % num
-			if factorOfN == 0 {
-				evenDivisors = append(evenDivisors, num)
-			}
-		}
-		if len(evenDivisors) == 11 {
-			fmt.Printf("%v is divisible by %v\n", i, evenDivisors)
-			break
-		}
-	}
+	fmt.Println(num)
 }
+
+// GCD
+func gcd(x, y int) int {
+	for y != 0 {
+		x, y = y, x%y
+	}
+	return x
+}
+
+// LCM can use GCD, so
+// LCM (m, n) = abs((m * n))/gcd(m, n)
