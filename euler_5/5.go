9a2cfde9df39865eb12fff2a2a10bab4b1107994
unknown <nchamberlain@BSBZGV1.beltman.local>
2014-08-06 09:07:25 -0400

    Finish 5, might optimize later

--- euler_5/5.go
+++ euler_5/5.go
@@ -5,19 +5,20 @@ import (
 )
 
 func main() {
-	lowNums := []int{
-		2, 3, 4, 5, 6, 7, 8, 9, 10,
+	divisors := []int{
+		10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
 	}
-	for i := 1; i < 3000; i++ {
+	for i := 1; ; i++ {
 		evenDivisors := make([]int, 0)
-		for _, num := range lowNums {
+		for _, num := range divisors {
 			factorOfN := i % num
 			if factorOfN == 0 {
 				evenDivisors = append(evenDivisors, num)
 			}
-			if len(evenDivisors) > 5 {
-				fmt.Printf("%v is divisible by %v\n", i, evenDivisors)
-			}
+		}
+		if len(evenDivisors) == 11 {
+			fmt.Printf("%v is divisible by %v\n", i, evenDivisors)
+			break
 		}
 	}
 }
