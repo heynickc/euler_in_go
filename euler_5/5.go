dd86be46ea3f0fd31e1690b36ace11b1173b21c8
unknown <nchamberlain@BSBZGV1.beltman.local>
2014-08-06 08:37:39 -0400

    Got basic 2520 answer narrowed down

--- euler_5/5.go
+++ euler_5/5.go
@@ -6,10 +6,18 @@ import (
 
 func main() {
 	lowNums := []int{
-		1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+		2, 3, 4, 5, 6, 7, 8, 9, 10,
 	}
-
-	for i, num := range lowNums {
-		fmt.Printf("%v index; %v num\n", i, num)
+	for i := 1; i < 3000; i++ {
+		evenDivisors := make([]int, 0)
+		for _, num := range lowNums {
+			factorOfN := i % num
+			if factorOfN == 0 {
+				evenDivisors = append(evenDivisors, num)
+			}
+			if len(evenDivisors) > 5 {
+				fmt.Printf("%v is divisible by %v\n", i, evenDivisors)
+			}
+		}
 	}
 }
